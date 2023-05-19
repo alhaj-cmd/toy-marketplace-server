2e4b36898fdf07d539ca040c4d078191c33e1c96
github_username <you@domain.com>
2023-05-20 01:24:53 +0600

    mongoDb add json and get client

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 console.log(process.env.DB_USER);
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.4s3yid7.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4s3yid7.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,12 +28,30 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toysCollection = client.db('toyMarket').collection('market')
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // get method
+    app.get('/toycategory', async(req,res) => {
+        const cursor = toysCollection.find()
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
+    // app.post("/postToy", async(req, res) => {
+    //     const body = req.body;
+    //     const result = await toysCollection.inserOne(body);
+    //     console.log(result);
+    //     res.send(result);
+    // } )
+
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
