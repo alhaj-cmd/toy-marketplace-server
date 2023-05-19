3c0179ed97acca8c55e6e4110ae6524a75b5cbaf
github_username <you@domain.com>
2023-05-19 15:02:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req,res) =>{
+    res.send('toy marketplace shop running');
+})
+
+app.listen(port,() =>{
+    console.log('toy marketplace server running')
+})
