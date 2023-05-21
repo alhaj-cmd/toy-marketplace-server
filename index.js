d70c7b6a98d8687c765609c37b6dd6711901efe0
github_username <you@domain.com>
2023-05-22 01:04:20 +0600

    deploy to vercel

--- index.js
+++ index.js
@@ -27,82 +27,85 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const toysCollection = client.db('toyMarket').collection('market')
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
+
     // get method
-    app.get('/toycategory/:category', async(req,res) => {
-      
-        if(req.params.category == "Science Toys" || req.params.category == 'Language Toys' || req.params.category == 'Math Toys') {
-            const cursor = toysCollection.find({subCategory: req.params.category})
+
+    app.get('/toycategory/:category', async (req, res) => {
+
+      if (req.params.category == "Science Toys" || req.params.category == 'Language Toys' || req.params.category == 'Math Toys') {
+        const cursor = toysCollection.find({ subCategory: req.params.category })
         const result = await cursor.toArray();
         console.log(result, cursor)
-       return res.send(result);
+        return res.send(result);
 
-        }
+      }
 
-        const cursor = toysCollection.find()
-        const result = await cursor.toArray();
-        res.send(result);
+      const cursor = toysCollection.find()
+      const result = await cursor.toArray();
+      res.send(result);
     })
 
-//  all category 
-app.get('/allCategory', async(req,res) =>{
-  const cursor = toysCollection.find();
-  const result = await cursor.toArray()
-  res.send(result);
-})
+    //  all category 
+    app.get('/allCategory', async (req, res) => {
+      const cursor = toysCollection.find();
+      const result = await cursor.toArray()
+      res.send(result);
+    })
 
-// single toy details
-app.get('/toycategory/:id', async(req, res) =>{
-  const id  = req.params.id;
-  const query = { _id: new ObjectId(id) }
 
-  const options = {
-    projection: {
-      toyName: 1, seller: 1, price: 1, subCategory: 1, rating:1, picture:1, description:1, availableQuantity:1
-    }
-  }
+    
+    // single toy details
+    app.get('/allCategory/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id)
+      const query = { _id: new ObjectId(id) }
 
-  const result = await toysCollection.findOne(query, options)
-  res.send(result);
-} )
+      const options = {
+        projection: { toyName: 1, seller: 1, price: 1, subCategory: 1, rating: 1, picture: 1, description: 1, availableQuantity: 1 },
+      };
+
+      const result = await toysCollection.findOne(query, options)
+      console.log("all data show single data", result);
+      res.send(result);
+    })
 
 
 
     // my toys
-    app.get('/myToys/:email', async(req, res) =>{
-      console.log('hitting the route',req.params.email);
-      const result = await toysCollection.find({postedBy : req.params.email }).toArray();
+    app.get('/myToys/:email', async (req, res) => {
+      const result = await toysCollection.find({ postedBy: req.params.email }).toArray();
       res.send(result)
     })
 
-    app.post('/postToy', async(req, res) => {
-        const body = req.body;
-        const result = await toysCollection.insertOne(body);
-        console.log(result);
-        res.send(result);
-    } )
-// Edit api call
-app.patch("/myToys/:id", async(req, res) => {
-  const id  = req.params.id;
-  const filter  = { _id: new ObjectId(id) }
-  const updateCategory = req.body
-  console.log(updateCategory);
-  const updateCategories = {
-    $set: {
-      status : updateCategory.status
-    },
-  };
-  const result = await toysCollection.updateOne(filter, updateCategories)
-  res.send(result);
-} )
+    app.post('/postToy', async (req, res) => {
+      const body = req.body;
+      const result = await toysCollection.insertOne(body);
+      console.log(result);
+      res.send(result);
+    })
+    // Edit api call
+    app.patch("/myToys/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updateCategory = req.body
+      console.log(updateCategory);
+      const updateCategories = {
+        $set: {
+          status: updateCategory.status
+        },
+      };
+      const result = await toysCollection.updateOne(filter, updateCategories)
+      res.send(result);
+    })
     // Delete Api call 
-    app.delete('/myToys/:id', async (req, res)  => {
+    app.delete('/myToys/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const result = await toysCollection.deleteOne(query)
@@ -118,10 +121,10 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req,res) =>{
-    res.send('toy marketplace shop running');
+app.get('/', (req, res) => {
+  res.send('toy marketplace shop running');
 })
 
-app.listen(port,() =>{
-    console.log('toy marketplace server running')
+app.listen(port, () => {
+  console.log('toy marketplace server running')
 })
